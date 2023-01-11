bf2a47fd009ae8445daee20ce48cfb1eef8cb15d
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2023-01-11 17:28:10 +0100

    added some posters

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ let posters = [
     { id: '1056604494', title: '', year: '' },
     { id: '1056604549', title: '', year: '2017' },
     { id: '2456547764', title: '', year: '' },
+    { id: '2768544954', title: '', year: '' },
     { id: '3454533987', title: '', year: '' },
     { id: '4554645331', title: '', year: '' },
     { id: '6567883433', title: '', year: '' },
     { id: '6757863435', title: '', year: '' },
     { id: '7657873434', title: '', year: '' },
+    { id: '7665354565', title: '', year: '' },
     { id: '8965534113', title: '', year: '' },
+    { id: '9032454334', title: '', year: '' },
     { id: '9354695984', title: '', year: '' },
     { id: '9657456564', title: '', year: '' },
+    { id: '9997874534', title: '', year: '' },
 ];
 
 
